cb265d3fd9e43732ec52cbaf14fdc8f35530f2ab
Minjun Choi <mi478@naver.com>
2022-11-28 23:59:15 +0900

    인스타 클론코딩

--- lib/src/controller/bottom_nav_controller.dart
+++ lib/src/controller/bottom_nav_controller.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_inst/src/components/message_popup.dart';
 import 'package:flutter_inst/src/pages/upload.dart';
 import 'package:get/get.dart';
 
@@ -11,32 +15,52 @@ class BottomNavController extends GetxController {
     var page = PageName.values[value]; // 위에 pagename을 0번째 1번째 이렇게 가져온다.
     switch (page) {
       case PageName.UPLOAD:
-      Get.to(()=>const Upload());
+        Get.to(() => const Upload());
         break;
       case PageName.HOME:
       case PageName.SEARCH:
       case PageName.ACTIVITY:
       case PageName.MYPAGE:
-        _changePage(value, hasGesture: hasGesture); /* hasGesture 알아보기 */
+        _changePage(value, hasGesture: hasGesture);
+        /* hasGesture 알아보기 */
         break;
     }
   }
+
   void _changePage(int value, {bool hasGesture = true}) {
     pageIndex(value);
-    if(!hasGesture) return;
+    if (!hasGesture) return;
+    if (bottomHistory.last != value){
     bottomHistory.add(value);
-    print(bottomHistory);
+    // print(bottomHistory);
+    }
+    //   if (bottomHistory.contains(value)) {
+    //   bottomHistory.remove(value);
+    // }
+    // [0, 1, 3, 4, 0] 으로 할려면?
   }
 
   Future<bool> willPopAction() async {
-    if(bottomHistory.length == 1) {
+    if (bottomHistory.length == 1) {
+      showDialog(
+          context: Get.context!,
+          builder: (context)=> MessagePopup(
+            message: '종료하시겠습니까?',
+            okCallback: () {
+              exit(0);
+            },
+            cancelCallback: Get.back,
+            title: '시스템',
+          ),
+      );
       print('exit!');
       return true;
-    }else{
+    } else {
       print('goto before page!');
       bottomHistory.removeLast();
       var index = bottomHistory.last;
       changeBottomNav(index, hasGesture: false);
+      // print(bottomHistory);
       return false;
     }
   }
