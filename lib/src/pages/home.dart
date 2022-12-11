29c0dcac5c9f3f63e2dec43ca7ca995d2c64e94d
Minjun Choi <mi478@naver.com>
2022-12-11 23:49:47 +0900

    인스타 클론코딩

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inst/src/components/image_data.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  Widget _storyBoadrList() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(
+          100,
+          (index) => Container(
+            width: 60,
+            height: 60,
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 1,
-        title: Container(),
-        actions: [],
-      ),
-    );
+        appBar: AppBar(
+          elevation: 1,
+          title: ImageData(IconsPath.logo, width: 270),
+          actions: [
+            GestureDetector(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: ImageData(
+                  IconsPath.directMessage,
+                  width: 50,
+                ),
+              ),
+            )
+          ],
+        ),
+        body: ListView(
+          children: [
+            _storyBoadrList(),
+            // _postList(),
+          ],
+        ));
   }
 }
